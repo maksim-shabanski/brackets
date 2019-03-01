c3e4341f140c98f86cb705f37521ebb85be949c9
Maksim Shabanski <shabanskij.maksim@gmail.com>
2019-03-01 21:52:47 +0300

    Solution of the brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var bracketMapping = {};
+  var brackets = str.split("");
+  var bracketsLength = brackets.length;
+  var openingBracketsStack = [];
+
+  if (bracketsLength % 2 !== 0) return false;
+
+  bracketsConfig.forEach(function(config) {
+    var openBracket = config[0];
+    var closeBracket = config[1];
+
+    bracketMapping[closeBracket] = openBracket;
+  });
+
+  for (var i = 0; i < bracketsLength; i++) {
+    var bracket = brackets[i];
+    var stackLength = openingBracketsStack.length;
+
+    if (
+      stackLength === 0 ||
+      openingBracketsStack[stackLength - 1] !== bracketMapping[bracket]
+    ) {
+      openingBracketsStack.push(bracket);
+    } else {
+      openingBracketsStack.pop();
+    }
+  }
+
+  return !openingBracketsStack.length;
+};
